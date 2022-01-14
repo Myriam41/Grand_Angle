63c7eb53c7a133cd44685506188f432b02d50ddf
Myriam41 <lieninformatique9@gmail.com>
2022-01-14 18:59:10 +0100

    css homeview in sass

--- private/controller/homeController.php
+++ private/controller/homeController.php
@@ -3,6 +3,5 @@
     include_once('connect.php');
     
     include('private/model/artWorkModel.php');
-    include('private/model/statsModel.php');
     include('private/model/expoModel.php');
     include('private/view/homeView.php');
