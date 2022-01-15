314971a4d9944a2b58a124412e7d46e5de00d14f
Myriam41 <lieninformatique9@gmail.com>
2022-01-15 22:46:11 +0100

    edit controller

--- private/controller/controller.php
+++ private/controller/controller.php
@@ -1,12 +1,13 @@
 <?php
+    include_once('connect.php');
 
     require('private/model/artWorkModel.php');
     require('private/model/connectModel.php');
     require('private/model/expoModel.php');
 
     function home(){
-
-        require('view/homeView.php');
+        
+        require('private/view/homeView.php');
     }
 
     function artsList(){
