0fc9514aa5fab3177329cb4feaad7b55f38d8e57
Myriam41 <lieninformatique9@gmail.com>
2022-01-17 17:16:15 +0100

    restore material icone

--- private/view/template/base.php
+++ private/view/template/base.php
@@ -18,9 +18,9 @@
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet">
 		<link href="'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap'" rel="stylesheet">
 
-		<!-- CDN Material icons
+		<!-- CDN Material icons        -->
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp">
-        -->
+
         <!-- CSS PERSO -->
         <link rel="stylesheet" href="assets/css/style.css">
                 
