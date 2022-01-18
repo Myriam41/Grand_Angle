8c36f214f62a335620fe9b9858c277735b368a57
Myriam41 <lieninformatique9@gmail.com>
2022-01-18 09:24:51 +0100

    add template right top

---
+++ private/view/template/right.php
@@ -0,0 +1,18 @@
+<div class="top">
+    <button id="menu-btn">
+        <span class="material-icons-sharp">menu</span>
+    </button>
+    <div class="theme-toggler">
+        <span class="material-icons-sharp active">light_mode</span>
+        <span class="material-icons-sharp">dark_mode</span>
+    </div>
+    <div class="profile">
+        <div class="info">
+            <p>Bonjour, <b>Mathieu</b></p>
+            <small class="text-muted">Admin</small>
+        </div>
+        <div class="profile-photo">
+            <img src="../img/261746878_453672526326290_2022088434576343659_n.jpg" alt="user_picture">
+        </div>
+    </div>
+</div>
