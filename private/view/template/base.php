f63a6b40bedfc5eb30054158b371d96eeb99d246
Myriam41 <lieninformatique9@gmail.com>
2022-02-15 19:23:25 +0100

    open modales ok

--- private/view/template/base.php
+++ private/view/template/base.php
@@ -55,7 +55,7 @@
 		<script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
 		<script type="text/javascript">(function(){window['__CF$cv$params']={r:'6d61d84b18bd3bda',m:'DibEAyWxF1uIQSCsAtEtwgvDO80Px5e.kubcBZgUWfs-1643620625-0-AaCStdCXDGxx4jAPlblOBhdO3mumKbJx/6QfpV4mG3jVB/T0ZSorJSF+tAF4QjX1Kn7By2ODpTbJMFAi0oaZMRJTu8dY4GsXhPwtACKLVhfVZNr/LminCvEczshg/QqXy1Yu9OJCrhGtrgs/VIo/jtlJkbrbS6sUdF8oumPMifRCnyzzMplAUcs27L1nB/7+8A==',s:[0xcb98f28c52,0x23fa878644],}})();</script>
 		<!-- Icones -->
-		<script async src='/cdn-cgi/bm/cv/669835187/api.js'></script>
+		<!-- <script async src='/cdn-cgi/bm/cv/669835187/api.js'></script> -->
 		<script src="assets/js/getData.js"></script>
 		<script src="assets/js/delete.js"></script>
 		<script src="assets/js/open.js"></script>
